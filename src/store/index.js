bca144446c5900bc2f57553c7879757c53042f60
Sanja <codesanja@gmail.com>
2018-10-23 16:13:05 +0100

    Clean up

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import C from '../constants'
-import appReducer, { goal } from './reducers'
+import appReducer from './reducers'
 import { createStore, applyMiddleware } from 'redux'
 
 const cosnoleMessages = store => next => action => {
@@ -7,13 +6,13 @@ const cosnoleMessages = store => next => action => {
     let result
 
     console.groupCollapsed(`dispatching action => ${action.type}`)
-    console.log('ski days', store.getState().allSkiDays.length)
+    console.log('Before dispatching ---> ski days', store.getState().allSkiDays.length)
 
     result = next(action)
 
     let { allSkiDays, goal, errors, resortNames } = store.getState()
 
-    console.log(`
+    console.log(`After dispatching ---> 
         ski days: ${allSkiDays.length}
         goal: ${goal}
         fetching: ${resortNames.fetching}
